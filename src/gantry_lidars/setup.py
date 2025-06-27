16d07df8689df2d4826276506de1582200eedca8
ryan123rudder <ryan123rudder@gmail.com>
2025-06-27 11:47:32 -0600

    watchdog integration into package

--- src/gantry_lidars/setup.py
+++ src/gantry_lidars/setup.py
@@ -21,6 +21,7 @@ setup(
     entry_points={
         'console_scripts': [
 		'gantry_lidars = gantry_lidars.PointCloudMerger:main'
+        'lidar_watchdog = gantry_lidars.lidar_watchdog:main'
         ],
     },
 )
